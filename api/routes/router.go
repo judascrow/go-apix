7b77554b1741681e9d898fb9d7de30c79a95561e
judascrow <judascrow1@gmail.com>
2020-04-17 15:32:09 +0700

    [Add] prometheus

--- api/routes/router.go
+++ api/routes/router.go
@@ -3,20 +3,38 @@ package routes
 import (
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/judascrow/go-api-crud/api/middlewares"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
 func InitRouter() *gin.Engine {
 
 	r := gin.New()
+
+	// Prometheus
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(r)
+	p.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
+		url := c.Request.URL.Path
+		for _, p := range c.Params {
+			if p.Key == "id" {
+				url = strings.Replace(url, p.Value, ":id", 1)
+				break
+			}
+		}
+		return url
+	}
+
 	r.HandleMethodNotAllowed = true
 	r.NoMethod(func(c *gin.Context) {
 		c.JSON(http.StatusMethodNotAllowed, gin.H{"success": false, "error": "Method Not Allowed"})
 		return
 	})
 
+	// middlewares
 	r.Use(middlewares.CustomLoggerZap(), middlewares.CorsMiddleware())
 	if os.Getenv("APP_ENV") == "dev" {
 		gin.ForceConsoleColor()
@@ -24,6 +42,7 @@ func InitRouter() *gin.Engine {
 	}
 	r.Use(gin.Recovery())
 
+	// Routes
 	apiv1 := r.Group(os.Getenv("APP_API_BASE_URL"))
 
 	apiv1.GET("/healthcheck", healthcheck)
